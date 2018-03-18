bc58953f0ff643147fd76aa70649b41a014d5ecb
HarryZhang1 <zhang.peng@grabtaxi.com>
2018-03-18 17:16:41 +0800

    just for testing

--- packages/eslint-config-airbnb/test/requires.js
+++ packages/eslint-config-airbnb/test/requires.js
@@ -13,5 +13,5 @@ test('all entry points parse', (t) => {
   t.doesNotThrow(() => require('../legacy'), 'legacy does not throw');
   t.doesNotThrow(() => require('../whitespace'), 'whitespace does not throw');
 
-  t.end();
+  // t.end();
 });
